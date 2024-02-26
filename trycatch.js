ebd5e62daa9ef1b8250d6155d0742aabac85069b
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-02-26 12:21:42 -0300

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -1,13 +1,20 @@
-//declaração de variavél, onde ela tem vetores ao invez de um valor 
-var matriz = ["vetor1","vetor2","vetor3","vetor4","vetor5"]
-//variavel com o valor 1
-var posicao = 1;
+//duas variaveis que estao atribuindo determinado "valor"
+var variavel1 = "valor somente leitura";//const é outra forma de declarar uma variável que não pode ser mudada so lida
+var variavel2 = "valor editável";
 
-//estrutura de repetição que será executada se pocisão for menor q matrix 
-//legth= o número de vetores
-while (posicao < matriz.legth) {  
-   //no console aparecerá...
-    console.log(matriz[posicao]);
-   //a posição vai somar, posição = posição + 1 
-   posicao++;
+// se a variavel1 for mudada para "valor não permitido vai aparecer no console a frase abaixo"
+try {
+    variavel1="valor não permitido";
+    console.log("A variavel2 foi alterada para:", variavel1)
+} catch (e){
+    // se não funcionar vai aparecer um alerta de erro no console dizendo
+    console.error("Ops! ocorreu um erro",e);
+}
+
+//mesma coisa só muda para variavel2 ao invez da variavel1
+try {
+    variavel2="valor permitido";
+    console.log("A variavel2 foi alterada para:", variavel2)
+} catch (e){
+    console.error("Ops! ocorreu um erro",e);
 }
